4d4d4342952c055ceb59bd5b4b188b34caa08f09
Ian <ian.hrnk@gmail.com>
2020-11-29 13:11:53 -0400

    add intensidade luz

--- shader/phong.fs
+++ shader/phong.fs
@@ -32,18 +32,22 @@ void main()
 
 float CalcPointLight(vec3 light_pos, vec3 frag_pos, vec3 normal, vec3 view_dir)
 {
+  // Intensidade da Luz
+  float distancia = length(light_pos - frag_pos);
+  float light_intensity = 1.0 / (1.0 + 1.0 * distancia + 0.25 * (distancia * distancia));
+
   // Ambiente
-  float ambient = k_ambient;
+  float ambient = k_ambient * light_intensity;
 
   // Difusa
   vec3 light_direction = normalize(light_pos - frag_pos);
   float diff = max(dot(normal, light_direction), 0.0);
-  float diffuse = k_diffuse * diff;
+  float diffuse = k_diffuse * diff * light_intensity;
 
   // Especular
   vec3 reflect_direction = reflect(-light_direction, normal);
   float spec = pow(max(dot(view_dir, reflect_direction), 0.0), 32);
-  float specular = k_specular * spec;
+  float specular = k_specular * spec * light_intensity;
 
   return (ambient + diffuse + specular);
 }
